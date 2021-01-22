c6f1a8ea75dbef2171ad2ab48f0f6e6fda897655
fhtwl <15927027721@163.com>
2021-01-22 14:09:33 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# 保存所有的修改
+echo "执行命令：git add -A"
+git add -A
+
+# 把修改的文件提交
+echo "执行命令：commit -m 'deploy'"
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f https://github.com/fhtwl/socket.git master
+git push -f https://github.com/fhtwl/socket.git master
+
+# 提交成功
+echo "提交成功"
